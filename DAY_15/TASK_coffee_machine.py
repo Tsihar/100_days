49571c4e5ac444f89be56acc24811abf19597e06
Tsihar <tsitavets.ihar@gmail.com>
2024-02-24 15:41:44 +0300

    coffee_machine_v1.2

--- DAY_15/TASK_coffee_machine.py
+++ DAY_15/TASK_coffee_machine.py
@@ -10,6 +10,7 @@ MENU = {
         "ingredients": {
             "water": 50,
             "coffee": 18,
+            "milk": 0
         },
         "cost": 1.5,
     },
@@ -38,46 +39,67 @@ ingredients_start = {
     'money': 0
 }
 
-client_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
-def enough_ingredients(choice):
-    water = ingredients_start['water'] - MENU[choice]['ingredients']['water']
-    milk = ingredients_start['milk'] - MENU[choice]['ingredients']['milk']
-    coffee = ingredients_start['coffee'] - MENU[choice]['ingredients']['coffee']
+def get_report():
+    print(f"Water: {ingredients_start['water']}ml\n"
+          f"Milk: {ingredients_start['milk']}ml\n"
+          f"Coffee: {ingredients_start['coffee']}ml\n"
+          f"Money: ${profit}\n")
 
-    if water >= 0 and milk >= 0 and coffee >= 0:
-        return True
-    elif water < 0:
-        return False  # print("Sorry there is not enough water.")
-    elif milk < 0:
-        return False  # print("Sorry there is not enough milk.")
-    elif coffee < 0:
-        return False  # print("Sorry there is not enough coffee.")
+
+def subtract_ingredients(choice):
+    leftover_water = ingredients_start['water'] - MENU[choice]['ingredients']['water']
+    leftover_milk = ingredients_start['milk'] - MENU[choice]['ingredients']['milk']
+    leftover_coffee = ingredients_start['coffee'] - MENU[choice]['ingredients']['coffee']
+    return [leftover_water, leftover_milk, leftover_coffee]
+    # if water >= 0 and milk >= 0 and coffee >= 0:
+    #     return True
+    # elif water < 0:
+    #     return False  # print("Sorry there is not enough water.")
+    # elif milk < 0:
+    #     return False  # print("Sorry there is not enough milk.")
+    # elif coffee < 0:
+    #     return False  # print("Sorry there is not enough coffee.")
 
 
 def refund_money(quarter, dime, nickle, penny, beverage_cost):
     inserted_money = (0.25 * quarter) + (0.1 * dime) + (0.05 * nickle) + (0.01 * penny)
     result = inserted_money - beverage_cost
-    return result
+    return round(result, 2)
 
-
-is_enough_ingredients = enough_ingredients(client_choice)
 
 profit = 0
+is_enough_ingredients = True
 while is_enough_ingredients:
-    print("Please insert coins.")
+    client_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if client_choice == "report":
+        get_report()
+        client_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
-    quarters = int(input("how many quarters?: "))
-    dimes = int(input("how many dimes?: "))
-    nickles = int(input("how many nickles?: "))
-    pennies = int(input("how many pennies?: "))
+    if ingredients_start['water'] >= MENU[client_choice]['ingredients']['water'] \
+            and ingredients_start['milk'] >= MENU[client_choice]['ingredients']['milk'] \
+            and ingredients_start['coffee'] >= MENU[client_choice]['ingredients']['coffee']:
 
-    change = refund_money(quarters, dimes, nickles, pennies, MENU[client_choice]['cost'])
+        print("Please insert coins.")
 
-    if change >= 0:
-        profit += MENU[client_choice]['cost']
-        print(f'Here is {change} in change.\nHere is your {client_choice}. Enjoy!')
+        quarters = int(input("how many quarters?: "))
+        dimes = int(input("how many dimes?: "))
+        nickles = int(input("how many nickles?: "))
+        pennies = int(input("how many pennies?: "))
+
+        change = refund_money(quarters, dimes, nickles, pennies, MENU[client_choice]['cost'])
+
+        ingredients_start['water'] = subtract_ingredients(client_choice)[0]
+        ingredients_start['milk'] = subtract_ingredients(client_choice)[1]
+        ingredients_start['coffee'] = subtract_ingredients(client_choice)[2]
+
+        if change >= 0:
+            profit += MENU[client_choice]['cost']
+            print(f'Here is {change} in change.\nHere is your {client_choice}. Enjoy!')
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+
     else:
-        print("Sorry that's not enough money. Money refunded.")
+        print("Sorry there is not enough coffee.")
 
-# print(f"Here is ${change} in change.")
+
