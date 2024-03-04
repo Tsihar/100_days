15b054f2c142071bb062f80c70c1b8f9dda5a83f
Tsihar <tsitavets.ihar@gmail.com>
2024-03-04 15:18:29 +0300

    coffee_machine_v1.5(final)

--- DAY_16_oop_intro/main.py
+++ DAY_16_oop_intro/main.py
@@ -3,19 +3,19 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# menu_items = MenuItem()
 menu = Menu()
 coffee = CoffeeMaker()
 machine = MoneyMachine()
 
 is_play = True
 while is_play:
-    client_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    client_choice = input(f"What would you like? ({menu.get_items()}): ").lower()
     if client_choice == "off":
         is_play = False
     elif client_choice == "report":
         coffee.report()
         machine.report()
     else:
-        if coffee.is_resource_sufficient(client_choice):
-            machine.make_payment(menu.MenuItem)
+        beverage = menu.find_drink(client_choice)
+        if coffee.is_resource_sufficient(beverage) and machine.make_payment(beverage.cost):
+            coffee.make_coffee(beverage)
