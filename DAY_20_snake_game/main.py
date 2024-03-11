f387d784fe327485cabf3edbc11efee1507c3147
Tsihar <tsitavets.ihar@gmail.com>
2024-03-11 22:22:51 +0300

    day_20_SNAKE_move_snake_1.1

--- DAY_20_snake_game/main.py
+++ DAY_20_snake_game/main.py
@@ -5,7 +5,8 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Змейка')
-screen.tracer(0) # если передаем 0, то будет черный экран, пока не пропишем update для включения экрана
+# screen.tracer(0) # если передаем 0, то будет черный экран, пока не пропишем update для включения экрана
+screen.listen()
 
 snake = []
 start_positions = [(0, 0), (-20, 0), (-40, 0)] # сделали стартовые позиции через кортеж
@@ -19,10 +20,17 @@ for sn in start_positions:
 # screen.update() # включаем экран для отрисовки сразу всей змейки, а не кусками
 game_is_on = True
 while game_is_on: # в этом месте выполнения кода экран черный из-за tracer-a, ниже уже update отрисовывает паровоз
-    screen.update()  # отрисовали весь паровоз только после движения каждого сегмента
+    # screen.update()  # отрисовали весь паровоз только после движения каждого сегмента
     time.sleep(0.1)  # задерживаем, иначе код так быстро работает, что не успеваем даже увидеть змейку, так она быстро вперед убегает
-    for move in snake:
-        move.forward(20) # двинули каждый сегмент
+    # for move in snake: # не подходит этот кусок кода для реализации поворота
+    #     move.forward(20) # двинули каждый сегмент
+    for move in range(len(snake) - 1, 0, -1):
+        new_x_cor = snake[move - 1].xcor()
+        new_y_cor = snake[move - 1].ycor()
+        snake[move].goto(x=new_x_cor, y=new_y_cor)
+    snake[0].forward(20)
+    snake[0].left(90)
+
 
 
 screen.exitonclick()
