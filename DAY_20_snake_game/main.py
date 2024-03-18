bfa239c4a02ee58e7deea6ca072cd3d4811ce62d
Tsihar <tsitavets.ihar@gmail.com>
2024-03-18 23:53:01 +0300

    day_20_SNAKE_control_the_snake_via_OOP

--- DAY_20_snake_game/main.py
+++ DAY_20_snake_game/main.py
@@ -7,10 +7,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Змейка')
-# screen.tracer(0) # если передаем 0, то будет черный экран, пока не пропишем update для включения экрана
+screen.tracer(0) # если передаем 0, то будет черный экран, пока не пропишем update для включения экрана
 screen.listen()
 
 snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
 
 # screen.update() # включаем экран для отрисовки сразу всей змейки, а не кусками
 game_is_on = True
@@ -18,6 +24,8 @@ game_is_on = True
 while game_is_on: # в этом месте выполнения кода экран черный из-за tracer-a, ниже уже update отрисовывает паровоз
     screen.update()  # отрисовали весь паровоз только после движения каждого сегмента
     time.sleep(0.1)  # задерживаем, иначе код так быстро работает, что не успеваем даже увидеть змейку, так она быстро вперед убегает
+
+
 
     snake.move()
 
--- DAY_20_snake_game/snake.py
+++ DAY_20_snake_game/snake.py
@@ -1,17 +1,22 @@
 from turtle import Turtle
+# from turtle import Screen
+# screen = Screen()
 
 START_POSITIONS = [
     (0, 0), (-20, 0), (-40, 0)  # сделали стартовые позиции через кортеж
 ]
 MOVE_DISTANCE = 20
 # создаем константы для того, чтобы менять если что их в одном месте, а не везде по коду искать
-
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
         self.snake = []
         self.create_snake() # при создании объекта в конструкторе создается и сегмент змейки
-
+        self.head = self.snake[0] # вынесли голову отдельно, т к она поворачивает всю змейку
     def create_snake(self):
         for sn in START_POSITIONS:
             new_snake = Turtle(shape='square')
@@ -25,4 +30,21 @@ class Snake:
             new_x_cor = self.snake[move - 1].xcor()
             new_y_cor = self.snake[move - 1].ycor()
             self.snake[move].goto(x=new_x_cor, y=new_y_cor)  # каждый сегмент становится на место впереди стоящего
-        self.snake[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def left(self):
+        if self.head.heading() != RIGHT: # нельзя в противоположную сторону идти
+            self.head.setheading(LEFT)
+
+
+    def right(self):
+        if self.head.heading() != LEFT: # нельзя в противоположную сторону идти
+            self.head.setheading(RIGHT)
+
+    def up(self):
+        if self.head.heading() != DOWN: # нельзя в противоположную сторону идти
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP: # нельзя в противоположную сторону идти
+            self.head.setheading(DOWN)
