8958bc68efe42ee84db100828ff7cdf3cd0453f1
Tsihar <tsitavets.ihar@gmail.com>
2024-04-26 00:22:16 +0300

    day_25_PANDAS_states_task_v_2

--- DAY_25_CSV_FILES/TASK_states_quiz/states_quiz.py
+++ DAY_25_CSV_FILES/TASK_states_quiz/states_quiz.py
@@ -1,3 +1,4 @@
+import time
 import turtle
 
 import pandas
@@ -8,6 +9,7 @@ screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+screen.tracer(0)
 
 
 
@@ -15,14 +17,22 @@ turtle.shape(image)
 answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
 print(data.state)
 st = data[data.state == answer_state]
+st_index = data.index[data.state == answer_state].to_list()[0]
+print(st_index)
 print(st)
+coord = data[data.state == answer_state].x[st_index]
+print(coord)
 # score = 0
 # while score != 50:
 if answer_state in data.state:
     turtle.hideturtle()
     turtle.color("black")
-    turtle.goto(data[data.state == answer_state].x.index, data[data.state == answer_state].y.index)
-    turtle.write(f'{answer_state}', font=("Courier", 20, "normal"))
+    turtle.penup()
+    turtle.goto((data[data.state == answer_state].x[st_index], data[data.state == answer_state].y[st_index]))
+    turtle.write(f'{answer_state}', font=("Courier", 10, "normal"))
+    time.sleep(1)
+    screen.update()
+
 
 
 
