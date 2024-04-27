6c8325970bac70da9167c205e181b23f7374f9db
Tsihar <tsitavets.ihar@gmail.com>
2024-04-27 13:02:58 +0300

    day_25_PANDAS_states_task_v_3

--- DAY_25_CSV_FILES/TASK_states_quiz/states_quiz.py
+++ DAY_25_CSV_FILES/TASK_states_quiz/states_quiz.py
@@ -4,34 +4,25 @@ import turtle
 import pandas
 data = pandas.read_csv("50_states.csv")
 
+my_states = turtle.Turtle()
 screen = turtle.Screen()
 screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
-screen.tracer(0)
 
 
-
-
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
-print(data.state)
-st = data[data.state == answer_state]
-st_index = data.index[data.state == answer_state].to_list()[0]
-print(st_index)
-print(st)
-coord = data[data.state == answer_state].x[st_index]
-print(coord)
-# score = 0
-# while score != 50:
-if answer_state in data.state:
-    turtle.hideturtle()
-    turtle.color("black")
-    turtle.penup()
-    turtle.goto((data[data.state == answer_state].x[st_index], data[data.state == answer_state].y[st_index]))
-    turtle.write(f'{answer_state}', font=("Courier", 10, "normal"))
-    time.sleep(1)
-    screen.update()
+score = 0
+while score != 50:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
+    st_index = data.index[data.state == answer_state].to_list()[0]
+    if answer_state in data.state.to_list():
+        my_states.hideturtle()
+        my_states.color("black")
+        my_states.penup()
+        my_states.goto((data[data.state == answer_state].x[st_index], data[data.state == answer_state].y[st_index]))
+        my_states.write(f'{answer_state}', font=("Courier", 10, "normal"))
+        score += 1
 
 
 
@@ -44,5 +35,5 @@ if answer_state in data.state:
 #
 # turtle.onscreenclick(get_mouse_click_coor) #передает значения координат клика в ф-цию
 #
-# turtle.mainloop() # помогает держать окно открытым, даже если код кончился
+turtle.mainloop() # помогает держать окно открытым, даже если код кончился
 screen.exitonclick()
