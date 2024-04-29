d137934f0f60065b5bb29df50c0af06fca74841a
Tsihar <tsitavets.ihar@gmail.com>
2024-04-29 23:26:00 +0300

    day_25_PANDAS_states_task_v_4

--- DAY_25_CSV_FILES/TASK_states_quiz/states_quiz.py
+++ DAY_25_CSV_FILES/TASK_states_quiz/states_quiz.py
@@ -12,18 +12,24 @@ screen.addshape(image)
 turtle.shape(image)
 
 
-score = 0
-while score != 50:
-    answer_state = screen.textinput(title="Guess the State", prompt="What's another state name?").title()
-    st_index = data.index[data.state == answer_state].to_list()[0]
-    if answer_state in data.state.to_list():
-        my_states.hideturtle()
-        my_states.color("black")
-        my_states.penup()
-        my_states.goto((data[data.state == answer_state].x[st_index], data[data.state == answer_state].y[st_index]))
-        my_states.write(f'{answer_state}', font=("Courier", 10, "normal"))
-        score += 1
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state name?").title()
 
+    if answer_state == "Exit":
+        break
+
+    if answer_state in data.state.to_list() and answer_state not in guessed_states:
+        my_states.hideturtle()
+        my_states.penup()
+        st_row = data[data.state == answer_state]
+        my_states.goto(int(st_row.x), int(st_row.y))
+        my_states.write(f'{answer_state}', font=("Courier", 8, "normal"))
+        guessed_states.append(answer_state)
+
+missed_states =
+states_to_learn.csv
+screen.exitonclick()
 
 
 
@@ -35,5 +41,5 @@ while score != 50:
 #
 # turtle.onscreenclick(get_mouse_click_coor) #передает значения координат клика в ф-цию
 #
-turtle.mainloop() # помогает держать окно открытым, даже если код кончился
-screen.exitonclick()
+# turtle.mainloop() # помогает держать окно открытым, даже если код кончился
+
