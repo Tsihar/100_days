57bda6db7fb6be99f4391910f3c83de3c7a59360
Tsihar <tsitavets.ihar@gmail.com>
2025-10-08 23:55:33 +0300

    day_29_pass_manager_tkinter: passw generator

--- DAY_29_PASS_MANAGER_TKINTER_FILES/main.py
+++ DAY_29_PASS_MANAGER_TKINTER_FILES/main.py
@@ -1,17 +1,55 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+#Password Generator Project
+def gen_passw():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    pass_letters = [choice(letters) for _ in range(randint(8, 10))]  # choice и randint импортирован из random напрямую
+    pass_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    pass_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    pass_list = pass_letters + pass_symbols + pass_numbers
+    shuffle(pass_list)  # импортирован из random напрямую
+
+    password = "".join(pass_list)
+
+    # вставляем пароль в поле и копируем в буфер обмена через предустановленный пэкадж pyperclip
+    psw_input.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def save_pass():
-    with open("data.txt", mode="a") as file:
-        file.write(f"{website_input.get()} | {email_input.get()} | {psw_input.get()}\n")
-        website_input.delete(0, END)
-        email_input.delete(0, END)
-        psw_input.delete(0, END)
-        website_input.focus()
+    website = website_input.get()
+    email = email_input.get()
+    password = psw_input.get()
+
+    if not all([email, password]):
+        messagebox.showwarning(title="Warning!", message="Please don't leave any fields empty")
+    else:
+        # попап спрашивающий ок или нет сохранить пароль в файл
+        is_ok = messagebox.askokcancel(
+            title=website, message=f"Your email: {email}\nPassword: {password} \nIs it ok for you?")
+
+        if is_ok:
+            with open("data.txt", mode="a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+                website_input.delete(0, END)
+                email_input.delete(0, END)
+                psw_input.delete(0, END)
+                website_input.focus()
+
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -47,7 +85,7 @@ psw_input = Entry(width=23)
 psw_input.grid(column=1, row=3, columnspan=2, sticky="w")
 
 # Buttons
-generate_button = Button(text='Generate password')
+generate_button = Button(text='Generate password', command=gen_passw)
 generate_button.grid(column=2, row=3)
 
 add_button = Button(text='Add', width=35, command=save_pass)
