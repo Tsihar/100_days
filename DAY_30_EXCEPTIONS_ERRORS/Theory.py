83402dcb6b4d3ed4bd94d3ccdd7a4d6022445ea6
Tsihar <tsitavets.ihar@gmail.com>
2025-10-13 23:42:34 +0300

    day_30_exceptions: tasks > try - except - else - finally

--- DAY_30_EXCEPTIONS_ERRORS/Theory.py
+++ DAY_30_EXCEPTIONS_ERRORS/Theory.py
@@ -3,45 +3,39 @@
 # else: # Делай это, если не было исключений
 # finally: # Делай это при любых раскладах
 
-# FileNotFound
-try:
-    file = open("a_file.txt")
-    a_dict = {"key": "value"}
-    print(a_dict["kadabra"])
-except FileNotFoundError:
-    # выполняется только если файла выше нет
-    # если не указывать тип ошибки оставив "except:", то мы пропускаем ошибку KeyError в print()
-    # поэтому нельзя оставлять эксепшн голым
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_msg:
-    # Ловим KeyError (вылезет только если файл найдётся)
-    # Записываем текст ошибки в переменную error_msg и делаем красивый вывод
-    print(f"The key {error_msg} doesn't exist")
-    # вывод: The key 'kadabra' doesn't exist
-else:
-    # Выполнится только если блок try пройдет успешно,
-    # иначе закончится на ошибках FileNotFoundError или KeyError
-    content = file.read()
-    print(content)
-finally:
-    # Выполняется всегда, будь то ошибка или успешное прохождение блока try
-    file.close()  # файл открыт не в with поэтому надо закрывать иначе утекает память
-    print("File was closed")
+# Обработка стандартных ошибок
+# try:
+#     file = open("a_file.txt")
+#     a_dict = {"key": "value"}
+#     print(a_dict["kadabra"])
+# except FileNotFoundError:
+#     # выполняется только если файла выше нет
+#     # если не указывать тип ошибки оставив "except:", то мы пропускаем ошибку KeyError в print()
+#     # поэтому нельзя оставлять эксепшн голым
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_msg:
+#     # Ловим KeyError (вылезет только если файл найдётся)
+#     # Записываем текст ошибки в переменную error_msg и делаем красивый вывод
+#     print(f"The key {error_msg} doesn't exist")
+#     # вывод: The key 'kadabra' doesn't exist
+# else:
+#     # Выполнится только если блок try пройдет успешно,
+#     # иначе закончится на ошибках FileNotFoundError или KeyError
+#     content = file.read()
+#     print(content)
+# finally:
+#     # Выполняется всегда, будь то ошибка или успешное прохождение блока try
+#     file.close()  # файл открыт не в with поэтому надо закрывать иначе утекает память
+#     print("File was closed")
 
-# FileNotFound
-# with open("file.txt") as file:
-#     file.read()
+# Поднятие своих собственных ошибок
+height = float(input("Height: "))
+weight = int(input("Weight: "))
 
-# KeyError
-# dictionary = {"key": "value"}
-# value = dictionary["non_existing_key"]
+if height > 3:
+    # с помощью ключевого слова raise
+    raise ValueError("Height should not be over 3 meters.")
 
-# IndexError
-# fruit_list = ["Apple", "Banana", "Pear"]
-# fruit = fruit_list[3]
-
-# TypeError
-# text = "abc"
-# print(text + 5)
-
+bmi = weight / height ** 2
+print(bmi)
---
+++ DAY_30_EXCEPTIONS_ERRORS/task.py
@@ -0,0 +1,38 @@
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+#TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+# Мой варик
+# while word := input("Enter a word: ").upper():
+#     try:
+#         output_list = [phonetic_dict[letter] for letter in word]
+#     except KeyError:
+#         print("Sorry, use Only letters. Try again.")
+#     else:
+#         print(output_list)
+#         break
+
+
+# Ее варик
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, use Only letters. Try again.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
+
+
