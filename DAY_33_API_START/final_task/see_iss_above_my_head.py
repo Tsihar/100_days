1dcb9a306294b34e8bbf0e9c688b5940885b16a0
Tsihar <tsitavets.ihar@gmail.com>
2025-11-11 22:39:40 +0300

    day_33_api_start: final task 'iss above my head' finished

--- DAY_33_API_START/final_task/see_iss_above_my_head.py
+++ DAY_33_API_START/final_task/see_iss_above_my_head.py
@@ -14,14 +14,16 @@ def iss_is_in_my_location():
     data = response.json()
 
     iss_latitude = float(data["iss_position"]["latitude"])
+    print(iss_latitude)
     iss_longitude = float(data["iss_position"]["longitude"])
+    print(iss_longitude)
 
-    iss_lat_equals_mine = MY_LAT - 5 < iss_latitude < MY_LAT + 5
+    iss_lat_equals_mine = MY_LAT - 5 <= iss_latitude <= MY_LAT + 5
     iss_lng_equals_mine = MY_LONG - 5 < iss_longitude < MY_LONG + 5
-    return iss_lat_equals_mine, iss_lng_equals_mine
+    return iss_lat_equals_mine and iss_lng_equals_mine
 
 
-def get_current_time():
+def is_night():
     parameters = {
         "lat": MY_LAT,
         "lng": MY_LONG,
@@ -39,7 +41,7 @@ def get_current_time():
     actual_hour = time_now.hour
     print(time_now)
 
-    return sunset < actual_hour and actual_hour > sunrise
+    return actual_hour >= sunset or actual_hour <= sunrise
 #If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
@@ -48,17 +50,15 @@ def get_current_time():
 while True:
     time.sleep(5)
     iss = iss_is_in_my_location()
+    cur_time = is_night()
     print(iss)
-    if all(iss):
-        cur_time = get_current_time()
-        print(cur_time)
-        if cur_time:
-            with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
-                connection.starttls()
-                connection.login(user="appbrewery87@gmail.com", password="rtqw vcqi bxbn ebvr")
-                connection.sendmail(from_addr="appbrewery87@gmail.com",
-                                    to_addrs="app_brewery@yahoo.com",
-                                    msg="Subject:See the iss\n\nIt's currently above your head")
-    time.sleep(5)
+    print(cur_time)
+    if iss and cur_time:
+        with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user="appbrewery87@gmail.com", password="rtqw vcqi bxbn ebvr")
+            connection.sendmail(from_addr="appbrewery87@gmail.com",
+                                to_addrs="app_brewery@yahoo.com",
+                                msg="Subject:See the iss\n\nIt's currently above your head")
 
 
